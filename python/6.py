b5e7eed290abea45b80943e58a38e76f7fce74a6
suzhenjiang1031 <suzhenjiang1031@gmail.com>
2025-01-27 21:57:29 +0800

    6.py

--- python/6.py
+++ python/6.py
@@ -7,7 +7,7 @@ class Solution(object):
         num_set = set(nums)
         longest_streak = 0
 
-        for num in num_set:
+        for num in num_set: 
             if num - 1 not in num_set:
                 current_num = num
                 current_streak = 1
@@ -16,8 +16,7 @@ class Solution(object):
                     current_num += 1
                     current_streak += 1
             
-            longest_streak = max(longest_streak, current_streak)
+                longest_streak = max(longest_streak, current_streak)
 
         return longest_streak
-    
     
