3a4e2d3e016ef518c925603b65e14416605a748d
suzhenjiang1031 <suzhenjiang1031@gmail.com>
2025-02-05 19:59:09 +0800

    13.py

--- python/13.py
+++ python/13.py
@@ -1,31 +1,51 @@
+# 定义 TreeNode
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution(object):
     def buildTree(self, preorder, inorder):
         """
         :type preorder: List[int]
         :type inorder: List[int]
-        :rtype: Optional[TreeNode]
+        :rtype: TreeNode
         """
-        if not preorder or not inorder:
+        # 用哈希表存储中序遍历的值和索引
+        inorder_map = {val: idx for idx, val in enumerate(inorder)}
+        return self.helper(preorder, inorder_map, 0, 0, len(inorder)-1)
+    
+    def helper(self, preorder, inorder_map, pre_start, in_start, in_end):
+        # 递归终止条件
+        if pre_start >= len(preorder) or in_start > in_end:
             return None
         
-        # The root is the first element in preorder
-        root_val = preorder[0]
+        # 当前子树的根节点是 preorder[pre_start]
+        root_val = preorder[pre_start]
         root = TreeNode(root_val)
         
-        # Find the root in inorder to divide left and right subtrees
-        root_index_in_inorder = inorder.index(root_val)
+        # 在中序遍历中找到根节点的位置
+        root_idx = inorder_map[root_val]
         
-        # Elements before root_index_in_inorder in inorder are part of the left subtree
-        # Elements after root_index_in_inorder in inorder are part of the right subtree
-        left_inorder = inorder[:root_index_in_inorder]
-        right_inorder = inorder[root_index_in_inorder+1:]
+        # 计算左子树的大小（用于分割前序遍历数组）
+        left_size = root_idx - in_start
         
-        # Elements after the first element in preorder are for the left and right subtrees
-        left_preorder = preorder[1:1+len(left_inorder)]
-        right_preorder = preorder[1+len(left_inorder):]
+        # 递归构建左子树和右子树
+        root.left = self.helper(
+            preorder, 
+            inorder_map, 
+            pre_start + 1,          # 左子树根在前序中的位置
+            in_start,               # 左子树在中序中的起点
+            root_idx - 1            # 左子树在中序中的终点
+        )
         
-        # Recursively build left and right subtrees
-        root.left = self.buildTree(left_preorder, left_inorder)
-        root.right = self.buildTree(right_preorder, right_inorder)
+        root.right = self.helper(
+            preorder,
+            inorder_map,
+            pre_start + left_size + 1,  # 右子树根在前序中的位置
+            root_idx + 1,               # 右子树在中序中的起点
+            in_end                      # 右子树在中序中的终点
+        )
         
         return root
