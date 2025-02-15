11a742b14bfc03df90e895e20a61fb264252ff83
suzhenjiang1031 <suzhenjiang1031@gmail.com>
2025-02-15 19:49:04 +0800

    24.py

---
+++ python/24.py
@@ -0,0 +1,7 @@
+def calc(*args, **kwargs):
+    items = list(args) + list(kwargs.values())
+    result = 0
+    for item in items:
+        if type(item) in (int, float):
+            result += item
+    return result
