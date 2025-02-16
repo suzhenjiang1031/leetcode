1fdd1157e64bd63c124d35f72f4e4389f3f33b49
suzhenjiang1031 <suzhenjiang1031@gmail.com>
2025-02-16 18:01:26 +0800

    25.py

---
+++ python/25.py
@@ -0,0 +1,7 @@
+def calc(init_value, op_func, *args, **kwargs):
+    items = list(args) + list(kwargs.values())
+    result = init_value
+    for item in items:
+        if type(item) in (int, float):
+            result = op_func(result, item)
+    return result
